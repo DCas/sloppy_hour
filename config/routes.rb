bb6dd0c211f9e6f6c7b4fa63b5b3d0259777a00b
Dave Casinghino <davidcasinghino@gmail.com>
2015-03-26 16:40:37 -0400

    create User model/setup authentication with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  get 'deals/create'
+  devise_for :users
 
   resources :venues
-
   resources :deals
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'venues#index'
+  root 'deals#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
