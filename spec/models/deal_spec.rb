ccd36b326e7df8265061fcf63eff7ccc26d5b979
Dave Casinghino <davidcasinghino@gmail.com>
2015-04-13 19:23:20 -0400

    add test for deal_occurrence destruction on associated deal destruction; not passing

--- spec/models/deal_spec.rb
+++ spec/models/deal_spec.rb
@@ -7,4 +7,16 @@ describe Deal do
   it { should belong_to(:venue) }
   it { should have_one(:schedule) }
 
+  describe ".destroy" do
+    it "should destroy associated DealOccurrences" do
+      pre_create_total_occurrences = DealOccurrence.count
+      deal = FactoryGirl.create(:deal)
+      pre_destroy_total_occurrences = DealOccurrence.count
+
+      deal.destroy
+
+      expect(DealOccurrence.count).to eq(pre_create_total_occurrences)
+    end
+  end
+
 end
