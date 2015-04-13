603d7e5ec04f3aeb5724442b50842f696fe1672c
Dave Casinghino <davidcasinghino@gmail.com>
2015-04-13 19:11:17 -0400

    Add admin namespace

---
+++ app/controllers/admin/venues_controller.rb
@@ -0,0 +1,59 @@
+class Admin::VenuesController < ApplicationController
+  before_filter :set_venue, only: [:show, :edit, :update, :destroy]
+  before_filter :user_location
+  respond_to :html, :json
+
+  def index
+    @venues = Venue.all
+  end
+
+  def show
+  end
+
+  def new
+    @venue = Venue.new
+  end
+
+  def create
+    @venue = Venue.new(venue_params)
+    if @venue.save
+      redirect_to @venue
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @venue.update(venue_params)
+      redirect_to @venue
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @venue.delete
+    redirect_to venues_path
+  end
+
+  private
+  def venue_params
+    params.require(:venue).permit(:name, :street_number, :street, :city, :state, :zipcode, :country, :latitude, :longitude, :website, :google_place_id)
+  end
+
+  def set_venue
+    @venue = Venue.find(params[:id])
+  end
+
+  def location_query
+    if params[:search] && params[:search][:location]
+      params[:search][:location]
+    else
+      @lat_lng
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :venues
+  resources :venues do
+    collection do
+      get 'search'
+    end
+  end
+  
+  namespace :admin do
+    resources :venues, :deals
+  end
   resources :deals
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
