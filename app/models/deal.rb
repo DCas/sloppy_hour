331775136b97461812d48aec82c17e6cd60ba74a
Dave Casinghino <davidcasinghino@gmail.com>
2015-04-13 15:37:06 -0400

    remove has_one :schedule; association already set up by schedulable gem; was redeclaring without desired dependent: destroy option that is already set up by default by scheduleable gem

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -2,7 +2,6 @@ class Deal < ActiveRecord::Base
   acts_as_schedulable occurrences: :deal_occurrences
 
   belongs_to :venue
-  has_one :schedule, as: :schedulable
 
   validates :title, :venue, presence: true
 
