f1e4c301bc85b2bce76c8e2b5ad53a8a971a5228
Dave Casinghino <davidcasinghino@gmail.com>
2015-04-15 04:03:10 -0400

    add empty test for Deal.occurring_on(date)

--- spec/models/deal_spec.rb
+++ spec/models/deal_spec.rb
@@ -7,7 +7,15 @@ describe Deal do
   it { should belong_to(:venue) }
   it { should have_one(:schedule) }
 
-  describe ".destroy" do
+# Class methods
+  describe '.occurring_on(date)' do
+    pending
+    it 'should return deals with occurrences on given date' do
+    end
+  end
+
+# Instance methods
+  describe "#destroy" do
     it "should destroy associated DealOccurrences" do
       pre_create_total_occurrences = DealOccurrence.count
       deal = FactoryGirl.create(:deal)
