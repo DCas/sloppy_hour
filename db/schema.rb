b4a8aa1857218a8b4f5734c3ce21c21555d76a8b
Dave Casinghino <davidcasinghino@gmail.com>
2015-04-17 03:16:36 -0400

    add start_time and end_time to DealOccurrence; WIP

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150414190706) do
+ActiveRecord::Schema.define(version: 20150415164905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 20150414190706) do
     t.integer  "count"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.time     "start_time"
+    t.time     "end_time"
   end
 
   create_table "taggings", force: :cascade do |t|
