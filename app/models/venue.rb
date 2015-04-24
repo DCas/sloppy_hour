ef6fcd293c5323e312b3b352f61c7613047e7d84
David Casinghino <davidcasinghino@gmail.com>
2015-04-23 20:23:38 -0400

    remove unused/unneeded reverse geocoding config

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -3,31 +3,19 @@ require 'date_extensions'
 class Venue < ActiveRecord::Base
   has_many :deals, dependent: :destroy
   has_many :deal_occurrences, through: :deals
-  has_many :todays_deals, -> { occurring_on(Date.current) },
-                              :class_name => "Deal"
+  has_many :todays_deals, -> { occurring_on(Date.current) }, :class_name => "Deal"
 
   validates :city, :country, :name, :state, :street, :street_number, :zipcode, presence: true
-  
-  before_validation :ensure_geocoded unless :geocoded?
-
-  geocoded_by :address
-  
-  reverse_geocoded_by :latitude, :longitude do |venue,results|
-    if geo = results.first
-      venue.street_number = geo.street_number
-      venue.street        = geo.address
-      venue.city          = geo.city
-      venue.state         = geo.state
-      venue.zipcode     ||= geo.postal_code
-      venue.country       = geo.country_code
-    end
-  end
 
   scope :with_deals_on, ->(date) {
     joins(:deals => :deal_occurrences).
       where(deal_occurrences: {:date => date.all_day}).
       distinct(:venue)
   }
+  
+  before_validation :ensure_geocoded unless :geocoded?
+
+  geocoded_by :address
 
   def address
     street_number.to_s + ' ' + [street, city, state].compact.join(', ') + ' ' + zipcode.to_s
@@ -38,4 +26,3 @@ class Venue < ActiveRecord::Base
   end
 
 end
-
